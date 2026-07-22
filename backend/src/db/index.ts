45ceceac4f4aa6751dff75212d7dd81ab39b893e
laraproto <laraproto@proton.me>
2026-07-22 12:13:22 +0300

    enable code first migrator

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -1,7 +1,7 @@
-//import { join } from 'path';
+import { join } from 'path';
 
 import { drizzle } from 'drizzle-orm/bun-sql';
-//import { migrate } from 'drizzle-orm/bun-sql/migrator';
+import { migrate } from 'drizzle-orm/bun-sql/migrator';
 //import { drizzle } from 'drizzle-orm/node-postgres';
 import { sql } from 'bun';
 import * as schema from './schema';
@@ -10,6 +10,6 @@ import * as schema from './schema';
 
 export const db = drizzle({ client: sql, schema });
 
-//await migrate(db, { migrationsFolder: join(import.meta.dir, '../../drizzle') });
+await migrate(db, { migrationsFolder: join(import.meta.dir, '../../drizzle') });
 
 export { schema };
